de534afd388439424b710ce3bd257ab9d2658b15
Alex Olshansky <alex@creagenics.com>
2023-12-01 17:14:03 +0100

    Day 1: Trebuchet?! (part 2)

--- day-1/trebuchet.js
+++ day-1/trebuchet.js
@@ -2,9 +2,24 @@ const fs = require('fs');
 const data = fs.readFileSync('input.txt', { encoding: 'utf8' });
 const lines = data.trim().split(/\r?\n/);
 
+// --- Part One ---
+
 const sum = lines.reduce((acc, line) => {
   const num = line.replace(/\D/g, '');
   return acc + +(num[0] + num[num.length-1]);
 }, 0);
 
 console.log(sum);
+
+// --- Part Two ---
+
+const words = ['\\d', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+const re = new RegExp(`(?=(${words.join('|')}))`, 'g'); // a lookahead assertion, for overlapping matches ("oneight" -> "18")
+const anotherSum = lines.reduce((acc, line) => {
+  const m = [...line.matchAll(re)];
+  const first = +m[0][1] || words.indexOf(m[0][1]);
+  const last = +m[m.length-1][1] || words.indexOf(m[m.length-1][1]);
+  return acc + 10 * first + last;
+}, 0);
+
+console.log(anotherSum);
