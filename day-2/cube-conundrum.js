efc739e939d515f6eceeba6df37476af2be6e0ad
Alex Olshansky <alex@creagenics.com>
2023-12-02 13:44:56 +0100

    Day 2: Cube Conundrum (part 2)

--- day-2/cube-conundrum.js
+++ day-2/cube-conundrum.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const data = fs.readFileSync('input.txt', { encoding: 'utf8' });
 const games = data.trim().split(/\r?\n/);
 
+// --- Part One ---
+
 const limits = { red: 12, green: 13, blue: 14 };
 
 const sum = games.reduce((acc, game, index) => {
@@ -16,3 +18,18 @@ const sum = games.reduce((acc, game, index) => {
 }, 0);
 
 console.log(sum);
+
+// --- Part Two ---
+
+const powerSum = games.reduce((acc, game) => {
+  const cn = [...game.matchAll(/(\d+) (red|green|blue)/g)];
+
+  const fewest = { red: 0, green: 0, blue: 0 };
+  cn.forEach(([, n, c]) => {
+    fewest[c] = Math.max(+n, fewest[c]);
+  });
+
+  return acc + fewest.red * fewest.green * fewest.blue;
+}, 0);
+
+console.log(powerSum);
