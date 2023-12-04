1c0b92ea4cfdfa356c604e3d8141c64361769754
Alex Olshansky <alex@creagenics.com>
2023-12-04 11:19:20 +0100

    Day 4: Scratchcards (part 2)

--- day-4/scratchcards.js
+++ day-4/scratchcards.js
@@ -2,15 +2,30 @@ const fs = require('fs');
 const data = fs.readFileSync('input.txt', { encoding: 'utf8' });
 const cards = data.trim().split(/\r?\n/);
 
-let points = 0;
-
-cards.forEach(c => {
+const matches = cards.map(c => {
   const [winning, mine] = c.split(':')[1].split('|')
     .map(s => s.match(/\d+/g).map(a => +a));
+  return winning.filter(l => mine.includes(l)).length;
+});
 
-  const m = winning.filter(l => mine.includes(l)).length;
+// --- Part One ---
 
+let points = 0;
+
+matches.forEach(m => {
   if (m > 0) points += Math.pow(2, m-1);
 });
 
 console.log(points);
+
+// --- Part Two ---
+
+const copies = Array(cards.length).fill(1);
+let total = 0;
+
+matches.forEach((m, id) => {
+  total += copies[id];
+  for (let i = 1; i <= m; i++) copies[id + i] += copies[id];
+});
+
+console.log(total);
