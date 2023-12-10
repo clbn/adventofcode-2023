2588e8bf5cf6c1f314a53d401fd041a4313a8fa4
Alex Olshansky <alex@creagenics.com>
2023-12-10 16:57:13 +0100

    Day 10: Pipe Maze (part 2)

--- day-10/pipe-maze.js
+++ day-10/pipe-maze.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const data = fs.readFileSync('input.txt', { encoding: 'utf8' });
 const maze = data.trim().split(/\r?\n/);
+const height = maze.length;
+const width = maze[0].length;
 
 const directions = [[0, -1], [1, 0], [0, 1], [-1, 0]];
 
@@ -46,11 +48,38 @@ let [x, y] = [sx + dx, sy + dy];
 // Previous tile
 let [px, py] = [sx, sy];
 
+// --- Part One ---
+
 let length = 1;
+const loop = Array(height).fill(false).map(_ => Array(width).fill('.')); // Part Two
+loop[y][x] = maze[y][x]; // Part Two
 while (maze[y][x] !== 'S') {
   [px, py, x, y] = getNext(px, py, x, y);
   length++;
+  loop[y][x] = maze[y][x]; // Part Two
 }
 
 const farthest = length / 2;
 console.log(farthest);
+
+// --- Part Two ---
+
+// Replace S with appropriate pipe
+const [[nx1, ny1], [nx2, ny2]] = getStartNeighbors(sx, sy);
+loop[sy][sx] = Object.keys(pipes).find(p => {
+  const [[dx1, dy1], [dx2, dy2]] = pipes[p];
+  return (dx1 === nx1 && dy1 === ny1 && dx2 === nx2 && dy2 === ny2);
+});
+
+// Count enclosed tiles with even-odd rule
+let enclosed = 0;
+for (let y = 1; y < height - 1; y++) {
+  let verticals = 0;
+  for (let x = 0; x < width; x++) {
+    const here = loop[y][x];
+    if (here === '|' || here === 'F' || here === '7') verticals++;
+    if (here === '.' && verticals % 2) enclosed++;
+  }
+}
+
+console.log(enclosed);
