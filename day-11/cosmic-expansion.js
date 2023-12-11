66ead0b963bb876c3924951b88f0c0b19907bc7e
Alex Olshansky <alex@creagenics.com>
2023-12-11 17:41:10 +0100

    Day 11: Cosmic Expansion (part 1)

---
+++ day-11/cosmic-expansion.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const data = fs.readFileSync('input.txt', { encoding: 'utf8' });
+const image = data.trim().split(/\r?\n/);
+const height = image.length;
+const width = image[0].length;
+
+// Gather galaxies and find empty cols/rows
+const galaxies = [];
+const cols = Array(width).fill(true);
+const rows = Array(height).fill(true);
+for (let y = 0; y < height; y++) {
+  for (let x = 0; x < width; x++) {
+    if (image[y][x] === '#') {
+      galaxies.push({ x, y });
+      cols[x] = false;
+      rows[y] = false;
+    }
+  }
+}
+
+// Accumulate expansion
+for (let i = 0, shift = 0; i < width; i++) {
+  if (cols[i]) {
+    shift++;
+  } else {
+    cols[i] = shift;
+  }
+}
+for (let i = 0, shift = 0; i < height; i++) {
+  if (rows[i]) {
+    shift++;
+  } else {
+    rows[i] = shift;
+  }
+}
+
+// Expand universe
+for (const g of galaxies) {
+  g.x += cols[g.x];
+  g.y += rows[g.y];
+}
+
+// Sum up distances
+let sum = 0;
+for (let a = 0; a < galaxies.length; a++) {
+  for (let b = a + 1; b < galaxies.length; b++) {
+    const ga = galaxies[a], gb = galaxies[b];
+    sum += Math.abs(ga.x - gb.x) + Math.abs(ga.y - gb.y);
+  }
+}
+
+console.log(sum);
