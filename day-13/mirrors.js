91a9f9b8be4ef8e37e16601cb757c55ca9ccca30
Alex Olshansky <alex@creagenics.com>
2023-12-13 14:39:30 +0100

    Day 13: Point of Incidence (part 2)

--- day-13/mirrors.js
+++ day-13/mirrors.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const data = fs.readFileSync('input.txt', { encoding: 'utf8' });
 const patterns = data.trim().split(/\r?\n\r?\n/).map(p => p.split(/\r?\n/));
 
-const mirrorHo = (p) => {
+const mirrorHo = (p, fp) => {
   const h = p.length;
   for (let y = 1; y < h; y++) {
-    if (p[y] === p[y-1]) {
+    if (p[y] === p[y-1] && y !== fp) {
       let confirm = true;
       for (let y1 = y, y2 = y-1; y1 < h && y2 >= 0; y1++, y2--) {
         if (p[y1] !== p[y2]) { confirm = false; break; }
@@ -16,7 +16,7 @@ const mirrorHo = (p) => {
   return 0;
 };
 
-const mirrorVe = (p) => {
+const mirrorVe = (p, fp) => {
   const w = p[0].length;
   const h = p.length;
   const transposed = [];
@@ -24,14 +24,36 @@ const mirrorVe = (p) => {
     transposed.push('');
     for (let y = 0; y < h; y++) transposed[x] += p[y][x];
   }
-  return mirrorHo(transposed);
+  return mirrorHo(transposed, fp);
 };
 
-const mirror = (p) => {
-  const ho = mirrorHo(p);
+const mirror = (p, fp = 0) => {
+  const ho = mirrorHo(p, fp / 100);
   if (ho) return ho * 100;
-  return mirrorVe(p);
+  return mirrorVe(p, fp);
 }
+
+// --- Part One ---
 
 const sum = patterns.reduce((acc, p) => acc + mirror(p), 0);
 console.log(sum);
+
+// --- Part Two ---
+
+const mirrorDirty = (p) => {
+  const falsePositive = mirror(p);
+  const w = p[0].length;
+  const h = p.length;
+  for (let y = 0; y < h; y++) {
+    for (let x = 0; x < w; x++) {
+      const cleaned = [...p];
+      cleaned[y] = cleaned[y].slice(0, x) + (cleaned[y][x] === '#' ? '.' : '#') + cleaned[y].slice(x + 1);
+      const r = mirror(cleaned, falsePositive);
+      if (r > 0) return r;
+    }
+  }
+  return 0;
+};
+
+const sum2 = patterns.reduce((acc, p) => acc + mirrorDirty(p), 0);
+console.log(sum2);
