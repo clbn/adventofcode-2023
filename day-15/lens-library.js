dce607524dc879aaf5a53efc22e6d5f059f3c4cf
Alex Olshansky <alex@creagenics.com>
2023-12-15 11:16:20 +0100

    Day 15: Lens Library (part 2)

--- day-15/lens-library.js
+++ day-15/lens-library.js
@@ -13,5 +13,34 @@ const hash = (s) => {
   return c;
 };
 
+// --- Part One ---
+
 const sum = seq.reduce((acc, step) => acc + hash(step), 0);
 console.log(sum);
+
+// --- Part Two ---
+
+const boxes = Array(256).fill(null).map(_ => []);
+
+for (const step of seq) {
+  const [label, focal] = step.split(/[-=]/);
+  const bi = hash(label);
+  const li = boxes[bi].findIndex(l => l.label === label);
+  if (focal) {
+    if (li > -1) {
+      boxes[bi][li].focal = focal;
+    } else {
+      boxes[bi].push({ label, focal });
+    }
+  } else {
+    if (li > -1) {
+      boxes[bi].splice(li, 1);
+    }
+  }
+}
+
+const power = boxes.reduce((acc, box, bi) =>
+  acc + (bi + 1) * box.reduce((acc, lens, li) =>
+    acc + (li + 1) * lens.focal, 0), 0);
+
+console.log(power);
