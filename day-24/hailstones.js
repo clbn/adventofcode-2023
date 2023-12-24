eaf9b76ebcd7aaa0232fbff253b6ad22fbc8876f
Alex Olshansky <alex@creagenics.com>
2023-12-24 20:21:51 +0100

    Day 24: Never Tell Me The Odds (part 2)

--- day-24/hailstones.js
+++ day-24/hailstones.js
@@ -4,6 +4,8 @@ const hailstones = data.trim().split(/\r?\n/)
   .map(r => r.split(/[@,]/)
     .map(n => +n));
 
+// --- Part One ---
+
 const crossed = (a, b, range) => {
   const [ax, ay, az, adx, ady, adz] = a;
   const [bx, by, bz, bdx, bdy, bdz] = b;
@@ -34,3 +36,38 @@ for (let a = 0; a < hailstones.length - 1; a++) {
 }
 
 console.log(intersections);
+
+// --- Part Two ---
+
+(async () => {
+  const { init } = require('z3-solver');
+  const { Context } = await init();
+  const { Solver, Real } = new Context('main');
+
+  const x = Real.const('x');
+  const y = Real.const('y');
+  const z = Real.const('z');
+  const dx = Real.const('dx');
+  const dy = Real.const('dy');
+  const dz = Real.const('dz');
+
+  const solver = new Solver();
+  for (let i = 0; i < 3; i++) {
+    const [sx, sy, sz, sdx, sdy, sdz] = hailstones[i];
+    const t = Real.const(`t${i}`);
+    solver.add(t.ge(0));
+    solver.add(x.add(dx.mul(t)).eq(t.mul(sdx).add(sx)));
+    solver.add(y.add(dy.mul(t)).eq(t.mul(sdy).add(sy)));
+    solver.add(z.add(dz.mul(t)).eq(t.mul(sdz).add(sz)));
+  }
+
+  await solver.check();
+  const model = solver.model();
+  const rx = +model.eval(x);
+  const ry = +model.eval(y);
+  const rz = +model.eval(z);
+  const sum = rx + ry + rz;
+
+  console.log(sum);
+  process.exit();
+})();
